a2be16836d8bee6cf6a605086588ea97a29e4ea7
Fatima <fatima.mubasher@rtcleague.com>
2026-03-02 23:54:36 +0500

    Update permission.py

--- schemas/permission.py
+++ schemas/permission.py
@@ -1,10 +1,8 @@
 from pydantic import BaseModel
 
-# Incoming data when creating a permission
 class PermissionCreate(BaseModel):
     name: str
 
-# Response data when sending permission info
 class PermissionResponse(BaseModel):
     id: int
     name: str
